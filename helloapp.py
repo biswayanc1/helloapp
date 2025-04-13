705144d6c522bada1e570441eb5b96d497b0de46
biswayanc1 <biswayanc1@gmail.com>
2025-04-13 20:30:54 +0530

    Setup for prometheus

--- helloapp.py
+++ helloapp.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from prometheus_client import generate_latest, Counter
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Biswayan says hello!"
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype="text/plain")
